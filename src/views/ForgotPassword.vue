09e56a704491b5739a06472acfd9d3c372b6a124
Ajuka David <ajuka.zephiniah@gmail.com>
2022-04-06 01:32:11 +0200

    implemented loading spinner and modal in ForgotPassword.vue

--- src/views/ForgotPassword.vue
+++ src/views/ForgotPassword.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="reset-password">
-
+    <Modal v-if="modalActive" v-on:close-modal="closeModal"/>
+    <Loading v-if="loading"/>
     <div class="form-wrap">
       <form class="reset">
       <h2>Reset Password</h2>
@@ -17,16 +18,32 @@
       <div class="background"></div>
     </div>
   </div>
-
 </template>
 
 <script>
 import email from '../assets/Icons/envelope-regular.svg'
+import Modal from "@/components/Modal";
+import Loading from "@/components/Loading";
 
 export default {
   name: "ForgotPassword",
+  data(){
+    return{
+    email: null,
+      modalActive: false,
+      loading: null,
+      modalMessage:""
+  }},
   components: {
+    Loading,
+    Modal,
     email
+  },
+  methods: {
+    closeModal(){
+      this.modalActive = !this.modalActive
+      this.email = ''
+    }
   }
 }
 </script>
