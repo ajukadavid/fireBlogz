7570d1cc66d9106c79466ee39e5ded28abde6cf6
Ajuka David <ajuka.zephiniah@gmail.com>
2022-04-05 12:21:22 +0200

    added blog cards component below blog posts

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,36 +2,80 @@
   <div class="home">
     <BlogPost :post="welcomeScreen"/>
     <BlogPost :post="post" v-for="(post, index) in sampleBlogPost" :key="index"/>
+    <div class="blog-card-wrap">
+      <div class="container">
+        <h3>View More Recent Blogs</h3>
+        <div class="blog-cards">
+          <BlogCard :post="post" v-for="(post, index) in sampleBlogCards" :key="index"/>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 
 import BlogPost from "@/components/BlogPost";
+import BlogCard from "@/components/BlogCard";
+
 export default {
   name: "Home",
-  components: {BlogPost},
-  data(){
+  components: {BlogCard, BlogPost},
+  data() {
     return {
       welcomeScreen: {
         title: 'Welcome!',
         blogPost: 'Weekly blog articles with al things programming including HTML, CSS, Javascript and more. Register today to never miss a post',
         welcomeScreen: true,
-        photo:'coding'
+        photo: 'coding'
       },
       sampleBlogPost: [
         {
           title: "This is a filler Title",
-          blogHTML:"this is a filler blog post title!",
+          blogHTML: "this is a filler blog post title!",
           blogCoverPhoto: "beautiful-stories"
         },
         {
           title: "This is a filler Title",
-          blogHTML:"this is a filler blog post title!",
+          blogHTML: "this is a filler blog post title!",
           blogCoverPhoto: "designed-for-everyone"
         }
+      ],
+      sampleBlogCards: [
+        {
+          blogTitle: 'Blog Card #1',
+          blogCoverPhoto: 'stock-1',
+          blogDate: 'May 1, 2021'
+        },
+        {
+          blogTitle: 'Blog Card #2',
+          blogCoverPhoto: 'stock-2',
+          blogDate: 'May 1, 2021'
+        },
+        {
+          blogTitle: 'Blog Card #3',
+          blogCoverPhoto: 'stock-3',
+          blogDate: 'May 1, 2021'
+        },
+        {
+          blogTitle: 'Blog Card #4',
+          blogCoverPhoto: 'stock-4',
+          blogDate: 'May 1, 2021'
+        },
       ]
     }
   }
 };
 </script>
+
+
+<style lang="scss" scoped>
+
+.blog-card-wrap {
+  h3 {
+    font-weight: 300;
+    font-size: 20px;
+    margin-bottom: 32px;
+  }
+}
+</style>
