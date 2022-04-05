a1c703e115fe71a904fecd4cf025979e1b441714
Ajuka David <ajuka.zephiniah@gmail.com>
2022-04-05 12:37:52 +0200

    added update/register section. finished homepage

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -10,17 +10,26 @@
         </div>
       </div>
     </div>
+
+    <div class="updates">
+      <div class="container">
+        <h2>Never miss a post,Register for your free account today!</h2>
+        <router-link to="#" class="router-button">
+          Register for FireBlogs <Arrow class="arrow arrow-light" />
+        </router-link>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-
+import Arrow from '../assets/Icons/arrow-right-light.svg'
 import BlogPost from "@/components/BlogPost";
 import BlogCard from "@/components/BlogCard";
 
 export default {
   name: "Home",
-  components: {BlogCard, BlogPost},
+  components: {BlogCard, BlogPost, Arrow},
   data() {
     return {
       welcomeScreen: {
@@ -76,6 +85,41 @@ export default {
     font-weight: 300;
     font-size: 20px;
     margin-bottom: 32px;
+  }
+}
+
+.updates {
+  .container {
+    padding: 100px 25px;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    @media(min-width: 800px){
+      padding: 125px 25px;
+      flex-direction: row;
+    }
+
+    .router-button {
+      display: flex;
+      font-size: 14px;
+      text-decoration: none;
+
+      @media(min-width: 800px) {
+        margin-left: auto;
+      }
+    }
+
+    h2 {
+      font-weight: 300;
+      font-size: 32px;
+      max-width: 425px;
+      text-align: center;
+      text-transform: uppercase;
+      @media (min-width: 800px) {
+        text-align: initial;
+        font-size: 40px;
+      }
+    }
   }
 }
 </style>
