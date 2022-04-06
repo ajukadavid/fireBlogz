bfb99541f07bcba64b643970202b328b663bb50b
Ajuka David <ajuka.zephiniah@gmail.com>
2022-04-06 15:32:27 +0200

    implemented profile menu on navbar component

--- src/App.vue
+++ src/App.vue
@@ -30,7 +30,6 @@ export default {
       }
     })
     this.checkRoute()
-
   },
   mounted() {},
   methods: {
--- src/components/NavComponent.vue
+++ src/components/NavComponent.vue
@@ -11,6 +11,39 @@
           <router-link class="link" to="#">Create Post</router-link>
           <router-link class="link" :to="{name: 'Login'}">Login / Register</router-link>
         </ul>
+        <div class="profile" ref="profile" @click="toggleProfileMenu">
+          <span>{{this.$store.state.profileInitials}}</span>
+          <div class="profile-menu" v-show="profileMenu">
+            <div class="info">
+              <p class="Initials">{{ this.$store.state.profileInitials}}</p>
+              <div class="right">
+                <p>{{this.$store.state.profileFirstName}} {{this.$store.state.profileFirstName}} </p>
+                <p>{{this.$store.state.profileUsername}} </p>
+                <p>{{this.$store.state.profileEmail}} </p>
+              </div>
+            </div>
+            <div class="options">
+              <div class="option">
+                <router-link to="#" class="option">
+                  <userIcon class="icon"/>
+                  <p>Profile</p>
+                </router-link>
+              </div>
+              <div class="option">
+                <router-link to="#" class="option">
+                  <adminIcon class="icon"/>
+                  <p>Sign Out</p>
+                </router-link>
+              </div>
+              <div class="option">
+                <router-link to="#" class="option">
+                  <signOutIcon class="icon"/>
+                  <p>Profile</p>
+                </router-link>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </nav>
     <menuIcon @click="toggleMobileNav" class="menu-icon" v-show="mobile"/>
@@ -27,17 +60,24 @@
 
 <script>
 import menuIcon from '../assets/Icons/bars-regular.svg'
+import userIcon from '../assets/Icons/user-alt-light.svg'
+import adminIcon from '../assets/Icons/user-crown-light.svg'
+import signOutIcon from '../assets/Icons/sign-out-alt-regular.svg'
 
 export default {
   name: "NavComponent",
   components: {
-    menuIcon
+    menuIcon,
+    userIcon,
+    adminIcon,
+    signOutIcon
   },
   data() {
     return {
       mobile: null,
       mobileNav: null,
-      windowWidth: null
+      windowWidth: null,
+      profileMenu: null
     }
   },
 
@@ -61,6 +101,9 @@ created(){
     //function to open and close nav menu
     toggleMobileNav() {
       this.mobileNav = !this.mobileNav
+    },
+    toggleProfileMenu(){
+      this.profileMenu = !this.profileMenu
     }
   }
 }
@@ -117,54 +160,138 @@ header {
           margin-right: 0;
         }
       }
+
+      .profile {
+        position: relative;
+        cursor: pointer;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        width: 40px;
+        height: 40px;
+        border-radius: 50%;
+        color: white;
+        background-color: #303030;
+
+        .profile-menu {
+          position: absolute;
+          top: 60px;
+          right: 0;
+          width: 250px;
+          background-color: #303030;
+          box-shadow: 0 4px 6px -1px rgba(0, 0, 0, 0.1) 0 2px 4px -1px rgba(0, 0, 0, 0.06);
+
+
+          .info {
+            display: flex;
+            align-items: center;
+            padding: 15px;
+            border-bottom: 1px solid #fff;
+
+
+            .initials {
+              position: initial;
+              width: 40px;
+              height: 40px;
+              background-color: #fff;
+              color: #303030;
+              display: flex;
+              align-items: center;
+              justify-content: center;
+              border-radius: 50%;
+            }
+
+            .right {
+              flex: 1;
+              margin-left: 24px;
+
+              p:nth-child(1) {
+                font-size: 14px;
+              }
+
+              p:nth-child(2),
+              p:nth-child(3) {
+                font-size: 12px;
+              }
+            }
+          }
+
+          .options {
+            padding: 15px;
+
+            .option {
+              text-decoration: none;
+              color: #fff;
+              display: flex;
+              align-items: center;
+              margin-bottom: 12px;
+
+              .icon {
+                width: 18px;
+                height: auto;
+              }
+
+              p {
+                font-size: 14px;
+                margin-left: 12px;
+              }
+
+              &:last-child {
+                margin-bottom: 0px;
+              }
+            }
+          }
+        }
+      }
     }
-  }
 
-  .menu-icon {
-    cursor: pointer;
-    position: absolute;
-    top: 32px;
-    right: 25px;
-    height: 25px;
-    width: auto;
-  }
-
-  .mobile-nav {
-    padding: 20px;
-    width: 70%;
-    max-width: 250px;
-    display: flex;
-    flex-direction: column;
-    position: fixed;
-    height: 100%;
-    background-color: #303030;
-    top: 0;
-    left: 0;
-
-    .link {
-      padding: 15px 0;
-      color: #fff;
+    .menu-icon {
+      cursor: pointer;
+      position: absolute;
+      top: 32px;
+      right: 25px;
+      height: 25px;
+      width: auto;
     }
-  }
+
+    .mobile-nav {
+      padding: 20px;
+      width: 70%;
+      max-width: 250px;
+      display: flex;
+      flex-direction: column;
+      position: fixed;
+      height: 100%;
+      background-color: #303030;
+      top: 0;
+      left: 0;
+
+      .link {
+        padding: 15px 0;
+        color: #fff;
+      }
+    }
 
 
-  .mobile-nav-enter-active,
-  .mobile-nav-leave-active{
-    transition: all 1s ease;
-  }
+    .mobile-nav-enter-active,
+    .mobile-nav-leave-active {
+      transition: all 1s ease;
+    }
 
 
-  .mobile-nav-enter {
-    transform: translateX(-250px);
-  }
-  .mobile-nav-enter-to {
-    transform: translateX(0);
-  }
-  .mobile-nav-leave-to {
-    transform: translateX(-250px);
-  }
+    .mobile-nav-enter {
+      transform: translateX(-250px);
+    }
 
+    .mobile-nav-enter-to {
+      transform: translateX(0);
+    }
+
+    .mobile-nav-leave-to {
+      transform: translateX(-250px);
+    }
+
+  }
 }
-
 
 </style>
